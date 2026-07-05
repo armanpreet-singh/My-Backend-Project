f1165de34cb104004a91c62d1d8c44a4c2b92a8d
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-05 15:55:36 +0530

    Added JWT and bcrypt.

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Types } from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema(
   {
     videoFile: {
@@ -39,5 +39,9 @@ const videoSchema = new Schema(
     timestamps: true,
   }
 );
+
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema);
