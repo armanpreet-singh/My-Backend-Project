c33b4ebbb763edeb9dc63eb7850ef71e34b5a885
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-05 14:01:08 +0530

    Defined Video Model.

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,16 +1,43 @@
 import mongoose, { Schema, Types } from "mongoose";
 
-
-
-
-
-
-
-
-
-
-
-
-
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //stored and called through CLOUDINARY URL
+      required: true,
+    },
+    thumbnail: {
+      type: String, //stored and called through CLOUDINARY URL
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Video = mongoose.model("Video", videoSchema);
