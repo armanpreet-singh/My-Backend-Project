9ad5d92b6ea859d0b68d488ae377d21694f7ea1a
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-06 13:27:45 +0530

    Algo For Registring User.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler(async (req,res)=>{
-res.status(200).json({
-    Message : "ok"
-})
+// Get User Details From Frontend.
+// Validation Checks - Not Empty
+// Check If User Already Exists - Username, Email.
+// Check For Images And Avatars,
+// Upload Them To Cloudinary.
+// Create User Object - Create Entry In DB.
+// Remove Password And Refresh Token Field From Response.
+// Check For USer Creation.
+// Return res.
 })
 
 export {registerUser}
