47898e36a99888915454c35e7b347a3fa98b53a3
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-07 12:07:22 +0530

    Bug Fixes.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,10 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/users.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiRespone } from "../utils/ApiResponse.js"
+import { useEffect } from "react"
+
 
 const registerUser = asyncHandler(async (req,res)=>{
 // Get User Details From Frontend.
@@ -10,6 +16,24 @@ const registerUser = asyncHandler(async (req,res)=>{
 // Remove Password And Refresh Token Field From Response.
 // Check For USer Creation.
 // Return res.
+
+const{fullName, email, username, password}=req.body
+console.log("email : ",email );
+
+if([fullName, email, username, password].some((field)=>field?.trim()==="")){
+    throw new ApiError(400,"All Fields Are Required")
+}
+
+const existedUser = User.findOne({
+    $or : [{username},{email    }]
+})
+
+if(existedUser){
+    throw new ApiError(409,"User With email or username already exists")
+}
+
+
+
 })
 
 export {registerUser}
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { app } from "./app.js";
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server Is Running At Port : ${process.env.PORT}`);
+        console.log(`⚙️  Server Is Running At Port : ${process.env.PORT}`);
         
     })
 })
