0bcda68e36fcf9c504e36390cf8477f1962e6673
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-09 15:13:53 +0530

    Decoding Refresh Token To Generate New Refresh Token.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/users.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiRespone } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+
 
 const generateAccessAndRefreshTokens = async(userId)=>
 {
@@ -182,6 +184,29 @@ return res
 
 
 })
+
+const refreshAccessToken = asyncHandler(async(req, res)=>{
+  const incomingRefreshToken = req.cookies.
+  refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401, "Unauthorised Request!")
+  }
+  
+  const decodedToken = jwt.verify(
+    incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET
+  )
+
+const user = await User.findById(decodedToken?._id)
+
+if(!user){
+  throw new ApiError(401, "Invalid Refresh Token!")
+}
+
+
+
+}
+)
 
 export { registerUser,
    loginUser,
