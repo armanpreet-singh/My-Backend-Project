5403c158580ea8000dd5b81675323f158535a303
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-10 11:22:05 +0530

    Add getUserChannelProfile function to retrieve user channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -344,6 +344,58 @@ return res
 )
 })
 
+const getUserChannelProfile = asyncHandler(async(req, res)=>{
+
+const {username} = req.params
+
+if(!username.trim()){
+  throw new ApiError(400, "Username Is Missing!")
+}
+
+const channel = await User.aggregate([
+  {
+    $match: {
+      username : username?.toLowerCase()
+    }
+  },
+  {
+    $lookup: {
+      from : "subscriptions",
+      localField : "_id",
+      foreignField : "channel",
+      as : "subscribers"
+    }
+  },
+  {
+      $lookup: {
+      from : "subscriptions",
+      localField : "_id",
+      foreignField : "subscriber",
+      as : "subscribedTo"
+    }
+  },
+  {
+    $addFields: {
+      subscribersCount: {
+        $size : "$subscribers"
+      },
+      channelsSubscribedToCount: {
+        $size : "$subscribedTo"
+      },
+      isSubscribed: {
+        $cond : {
+          if : { $in : [req.user?._id, "$subscribers.subscriber "]},
+          then : true,
+          else : false
+        }
+      }
+    }
+  }
+])
+
+
+})
+
 
 export { registerUser,
    loginUser,
