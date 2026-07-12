b341fa6845ced226ebd7925ec8daaa2897e9e308
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-12 11:04:46 +0530

    Fix formatting and correct ApiResponse instantiation in healthcheck controller.

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,15 +1,16 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-   return res
-   .status(200)
- .json(new ApiRespone(200, {}, "OK" ))
-})
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {},
+            "OK"
+        )
+    );
+});
 
 export {
-    healthcheck
-    }
-    
+    healthcheck,
+};
